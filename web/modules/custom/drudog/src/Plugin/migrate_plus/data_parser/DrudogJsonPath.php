0e086c58a1c76eb76b38043d2153551594853f91
Michael Strelan <mstrelan@gmail.com>
2020-04-02 11:55:10 +1000

    Fix coding standards

--- web/modules/custom/drudog/src/Plugin/migrate_plus/data_parser/DrudogJsonPath.php
+++ web/modules/custom/drudog/src/Plugin/migrate_plus/data_parser/DrudogJsonPath.php
@@ -58,7 +58,7 @@ class DrudogJsonPath extends JsonPath {
    *   The associative array of source data.
    */
   private function alterSourceData(array &$source_data) {
-    foreach ($source_data as $key => $data) {
+    foreach ($source_data as $data) {
       if (isset($data->id)) {
         // Gather all the compound fields we need to alter.
         $fields = [
