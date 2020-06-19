6d082e6a50b8659fc2337745ef3febd413464285
Michael Strelan <mstrelan@gmail.com>
2020-06-19 16:12:13 +1000

    Use .env file for Github Actions config

--- .github/config/settings.local.php
+++ .github/config/settings.local.php
@@ -3,14 +3,13 @@
 /**
  * Local settings for GitHub Actions.
  */
-
 $databases['default']['default'] = [
-  'database' => 'drupal',
-  'username' => 'drupal',
-  'password' => 'drupal',
+  'database' => getenv('MYSQL_DATABASE'),
+  'username' => getenv('MYSQL_USER'),
+  'password' => getenv('MYSQL_PASSWORD'),
   'prefix' => '',
   'host' => 'mysql',
-  'port' => '',
+  'port' => getenv('MYSQL_PORT'),
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
 ];
